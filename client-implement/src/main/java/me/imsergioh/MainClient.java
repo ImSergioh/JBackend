6a3bffd6f6ba21148a76410a33d83109af4c2b99
ImSergioh <50914599+ImSergioh@users.noreply.github.com>
2023-11-29 07:33:37 +0100

    goin on

--- client-implement/src/main/java/me/imsergioh/MainClient.java
+++ client-implement/src/main/java/me/imsergioh/MainClient.java
@@ -1,22 +1,9 @@
 package me.imsergioh;
 
 import me.imsergioh.jbackend.BackendConnection;
-import me.imsergioh.jbackend.api.manager.DataProcessorManager;
-import me.imsergioh.listener.CacheSetListener;
-
-import java.io.IOException;
-import java.util.HashMap;
 
 public class MainClient {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         BackendConnection connection = new BackendConnection("127.0.0.1", 7777);
-        DataProcessorManager.register(new CacheSetListener());
-        connection.run();
-        HashMap<String, Integer> testMap = new HashMap<>();
-        testMap.put("user1", 10);
-        testMap.put("user2", 70);
-        testMap.put("user3", 7);
-        connection.send("String aidjsofiajsojds");
-        int count = 1;
     }
 }
